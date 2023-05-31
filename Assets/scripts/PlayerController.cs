a3fb6942f0110dbed6f356e231c948cbaba84567
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-31 11:37:20 +0100

    6.Mover-se em direção ao ponto focal

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    private GameObject focalPoint;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
         
     }
 
@@ -16,6 +18,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
