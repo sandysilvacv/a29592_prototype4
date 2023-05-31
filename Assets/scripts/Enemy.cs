c9c0800b53da3b069d4b1b98b1bbdbf1a642b17c
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-31 22:10:36 +0100

    2.Crie script inimigo para seguir o jogador

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
