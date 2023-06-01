b8832667f03ac2b40b3de1110c659e0d7861374b
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-06-01 22:17:31 +0100

    6.Adicione um indicador de ativação

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private float powerUpStrength = 15.0f;
     private GameObject focalPoint;
     public bool hasPowerup = false;
+    public GameObject powerupIndicator;
     
     void Start()
     {
@@ -22,6 +23,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,8 +31,10 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup")) 
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
+           
         }
     }
 
@@ -38,6 +42,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
