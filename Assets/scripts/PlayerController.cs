a487fc6708035f49d1332dd0c21b820c8db35774
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-06-01 21:02:46 +0100

    2.Destrua o powerup na colisão

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -19,5 +20,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup")) 
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
